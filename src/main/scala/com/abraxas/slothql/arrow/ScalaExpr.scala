366975c1393c8edbf4fb988aee60dde653f3709b
Dmitry Kovalev <kdn.kovalev@gmail.com>
2018-10-20 16:37:11 -0500

    added Source and Target manifests to ScalaExpr

--- src/main/scala/com/abraxas/slothql/arrow/ScalaExpr.scala
+++ src/main/scala/com/abraxas/slothql/arrow/ScalaExpr.scala
@@ -6,21 +6,30 @@ import shapeless.tag.@@
 import shapeless.{ <:!<, Cached, HList, LUBConstraint, LabelledGeneric, ops }
 
 import com.abraxas.slothql.arrow.Arrow.Types
+import com.abraxas.slothql.util.ShowManifest
 
 
-sealed trait ScalaExpr extends Arrow with ScalaExpr.FieldSelectionOps
+sealed trait ScalaExpr extends Arrow with ScalaExpr.FieldSelectionOps {
+  val src: Manifest[Source]
+  val tgt: Manifest[Target]
+}
 
 object ScalaExpr {
   type Aux[-S, +T] = ScalaExpr { type Source >: S; type Target <: T }
 
-  def apply[A]: Id[A] = Id[A]
+  def apply[A: Manifest]: Id[A] = Id[A]
 
 
   /** Identity arrow. (self selection) */
-  sealed trait Id[A] extends ScalaExpr with Arrow.Id[A]
+  sealed trait Id[A] extends ScalaExpr with Arrow.Id[A] {
+    override def toString: String = s"Id[${ShowManifest(src)}]"
+  }
   object Id {
-    def apply[A]: Id[A] = instance.asInstanceOf[Id[A]]
-    private lazy val instance = new Id[Any]{}
+    def apply[A: Manifest]: Id[A] =
+      new Id[A]{
+        val src: Manifest[A] = manifest[A]
+        val tgt: Manifest[A] = manifest[A]
+      }
   }
 
 
@@ -42,6 +51,10 @@ object ScalaExpr {
       new Composition[ScalaExpr, ScalaExpr] {
         val F: ScalaExpr = f
         val G: ScalaExpr = g
+        type Source = G.Source
+        type Target = F.Target
+        val src: Manifest[G.Source] = G.src
+        val tgt: Manifest[F.Target] = F.tgt
       }
   }
 
@@ -54,47 +67,81 @@ object ScalaExpr {
   implicit def splitScalaExpr[Arrows <: HList, S, Ts <: HList, T](
     implicit
     areScalaExprs: LUBConstraint[Arrows, ScalaExpr],
-    canSplit: Arrow.Split.Splitter.CanSplit.Aux[Arrows, S, T]
+    canSplit: Arrow.Split.Splitter.CanSplit.Aux[Arrows, S, T],
+    sourceMf: Manifest[S],
+    targetMf: Manifest[T]
   ): Arrow.Split.Splitter.Aux[Arrows, Split.Aux[Arrows, S, T]] =
-    splitterInstance.asInstanceOf[Arrow.Split.Splitter.Aux[Arrows, Split.Aux[Arrows, S, T]]]
-
-  private lazy val splitterInstance = new Arrow.Split.Splitter[HList] {
-    type Out = Split[HList]
-    def apply(t: HList): Split[HList] = new Split[HList] { val arrows: HList = t }
-  }
+    new Arrow.Split.Splitter[Arrows] {
+      type Out = Split.Aux[Arrows, S, T]
+      def apply(t: Arrows): Split.Aux[Arrows, S, T] =
+        new Split[Arrows] {
+          val arrows: Arrows = t
+          type Source = S
+          type Target = T
+          val src: Manifest[S] = sourceMf
+          val tgt: Manifest[T] = targetMf
+        }
+    }
 
 
   /** Expression representing selection of a field of an ADT (case class). */
-  case class SelectField[Obj, K <: String, V](field: K)
-    extends ScalaExpr { type Source = Obj; type Target = V }
+  case class SelectField[Obj: Manifest, K <: String, V: Manifest](field: K) extends ScalaExpr {
+    type Source = Obj
+    type Target = V
+    val src: Manifest[Obj] = manifest[Obj]
+    val tgt: Manifest[V] = manifest[V]
+
+    override def toString: String = s"SelectField[${ShowManifest(src)}, ${ShowManifest(tgt)}]($field)"
+  }
 
   /** Expression representing functor `map` operation. */
-  case class FMap[F[_], E <: ScalaExpr](expr: E)(implicit val F: cats.Functor[F])
-    extends ScalaExpr { type Source = F[expr.Source]; type Target = F[expr.Target] }
+  case class FMap[F[_], E <: ScalaExpr](expr: E)(implicit val F: cats.Functor[F], mf: Manifest[F[_]]) extends ScalaExpr {
+    type Source = F[expr.Source]
+    type Target = F[expr.Target]
+    val src: Manifest[F[expr.Source]] = classType(mf, expr.src)
+    val tgt: Manifest[F[expr.Target]] = classType(mf, expr.tgt)
+
+    override def toString: String = s"FMap[${ShowManifest(src)}, ${ShowManifest(tgt)}]($expr)"
+  }
   object FMap {
     def mk[F[_]]: Builder[F] = Builder.asInstanceOf[Builder[F]]
 
     protected class Builder[F[_]] {
-      def apply[E <: ScalaExpr](expr: E)(implicit F: cats.Functor[F]): FMap[F, E] = FMap[F, E](expr)
+      def apply[E <: ScalaExpr](expr: E)(implicit F: cats.Functor[F], mf0: Manifest[F[_]]): FMap[F, E] = FMap[F, E](expr)
     }
     private object Builder extends Builder
   }
   
   /** Expression representing monadic bind / `flatMap` operation. */
-  case class MBind[F[_], E <: ScalaExpr](expr: E)(implicit val M: cats.Monad[F])
-    extends ScalaExpr { type Source = F[expr.Source]; type Target = expr.Target }
+  case class MBind[F[_], E <: ScalaExpr](expr: E)(implicit val M: cats.Monad[F], mf: Manifest[F[_]]) extends ScalaExpr {
+    type Source = F[expr.Source]
+    type Target = expr.Target
+    val src: Manifest[F[expr.Source]] = classType(mf, expr.src)
+    val tgt: Manifest[expr.Target] = expr.tgt
+
+    override def toString: String = s"MBind[${ShowManifest(src)}, ${ShowManifest(tgt)}]($expr)"
+  }
   object MBind {
     def mk[F[_]]: Builder[F] = Builder.asInstanceOf[Builder[F]]
 
     protected class Builder[F[_]] {
-      def apply[E <: ScalaExpr](expr: E)(implicit M: cats.Monad[F]): MBind[F, E] = MBind[F, E](expr)
+      def apply[E <: ScalaExpr](expr: E)(implicit M: cats.Monad[F], mf0: Manifest[F[_]]): MBind[F, E] = MBind[F, E](expr)
     }
     private object Builder extends Builder
   }
 
   // TODO
-  case class SelectIn[F[_], Sel, V](sel: Sel)
-    extends ScalaExpr { type Source = F[V]; type Target = V }
+  case class SelectIn[F[_], Sel, V](sel: Sel)(implicit mf: Manifest[F[_]], mv: Manifest[V]) extends ScalaExpr {
+    type Source = F[V]
+    type Target = V
+    val src: Manifest[F[V]] = classType(mf, mv)
+    val tgt: Manifest[V] = mv
+
+    override def toString: String = s"SelectIn[${ShowManifest(src)}, ${ShowManifest(tgt)}]($sel)"
+  }
+
+  private def classType[F[_], T](clazz: Manifest[_ <: F[_]], arg: Manifest[T]): Manifest[F[T]] =
+    Manifest.classType(clazz.runtimeClass.asInstanceOf[Class[F[T]]], arg)
 
 
   // // // // // // Syntax Ops // // // // // //
@@ -104,22 +151,24 @@ object ScalaExpr {
     implicit
     targetA: Types.Aux[A, _, TA],
     source0: TA <:< F[S0],
-    functor: cats.Functor[F]
+    functor: cats.Functor[F],
+    mf0: Manifest[F[_]]
   ) {
 
     def map[B <: ScalaExpr](b: B)(implicit compose: Arrow.Compose[FMap[F, B], A]): compose.Out = compose(new FMap[F, B](b), a)
-    def map[B <: ScalaExpr](fb: Id[S0] => B)(implicit compose: Arrow.Compose[FMap[F, B], A]): compose.Out = compose(new FMap[F, B](fb(Id[S0])), a)
+    def map[B <: ScalaExpr](fb: Id[S0] => B)(implicit compose: Arrow.Compose[FMap[F, B], A], mf: Manifest[S0]): compose.Out = compose(new FMap[F, B](fb(Id[S0])), a)
   }
 
   implicit class ScalaExprMBindOps[A <: ScalaExpr, F[_], TA, S0](a: A)(
     implicit
     targetA: Types.Aux[A, _, TA],
     source0: TA <:< F[S0],
-    monad: cats.Monad[F]
+    monad: cats.Monad[F],
+    mf0: Manifest[F[_]]
   ) {
 
     def flatMap[B <: ScalaExpr.Aux[_, F[_]]](b: B)(implicit compose: Arrow.Compose[MBind[F, B], A]): compose.Out = compose(new MBind[F, B](b), a)
-    def flatMap[B <: ScalaExpr.Aux[_, F[_]]](fb: Id[S0] => B)(implicit compose: Arrow.Compose[MBind[F, B], A]): compose.Out = compose(new MBind[F, B](fb(Id[S0])), a)
+    def flatMap[B <: ScalaExpr.Aux[_, F[_]]](fb: Id[S0] => B)(implicit compose: Arrow.Compose[MBind[F, B], A], mf: Manifest[S0]): compose.Out = compose(new MBind[F, B](fb(Id[S0])), a)
   }
 
   protected trait FieldSelectionOps extends Dynamic {
@@ -129,7 +178,9 @@ object ScalaExpr {
       implicit
       ev0: Syntax.HasField.Aux[Target, Symbol @@ k.type, V],
       ev1: expr.type <:< A, // using `expr.type` directly in `compose` would require _existential types_
-      compose: Arrow.Compose[SelectField[Target, k.type, V], A]
+      compose: Arrow.Compose[SelectField[Target, k.type, V], A],
+      sourceMf: Manifest[Target],
+      targetMf: Manifest[V]
     ): compose.Out = compose(SelectField(k), expr)
   }
 
