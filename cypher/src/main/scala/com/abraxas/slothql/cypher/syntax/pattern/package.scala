392c5b0c8288753a31353a033279a5ac60e75c6a
Dmitry Kovalev <kdn.kovalev@gmail.com>
2019-01-16 17:38:35 -0600

    [dynamic query pattern] inverse edges syntax;

--- cypher/src/main/scala/com/abraxas/slothql/cypher/syntax/pattern/package.scala
+++ cypher/src/main/scala/com/abraxas/slothql/cypher/syntax/pattern/package.scala
@@ -18,29 +18,61 @@ package object pattern {
             length: Pattern.Rel.type => Pattern.Rel.Length = null,
             alias:  String                                 = null
           ): PatternRightPathBuilder =
-      new PatternRightPathBuilder(pat, Pattern.Rel(Option(alias), types.toList, props, Option(length).map(_(Pattern.Rel)), _))
+      new PatternRightPathBuilder(pat, mkRel(alias, types, props, length, _))
+
+    def `<-`( types:  Seq[String]                            = Nil,
+              props:  Map[String, Known[Expr[_]]]            = Map(),
+              length: Pattern.Rel.type => Pattern.Rel.Length = null,
+              alias:  String                                 = null
+            ): PatternLeftPathBuilder = new PatternLeftPathBuilder(pat, mkRel(alias, types, props, length, Pattern.Rel.Incoming))
+
+    /** Unicode alias for [[<-]]. Has different operator precedence. */
+    def ⟵( types:  Seq[String]                            = Nil,
+             props:  Map[String, Known[Expr[_]]]            = Map(),
+             length: Pattern.Rel.type => Pattern.Rel.Length = null,
+             alias:  String                                 = null
+           ): PatternLeftPathBuilder = `<-`(types, props, length, alias)
+
+    private def mkRel(alias: String, types: Seq[String], props: Map[String, Known[Expr[_]]], length: Pattern.Rel.type => Pattern.Rel.Length, dir: Pattern.Rel.Direction) =
+      Pattern.Rel(Option(alias), types.toList, props, Option(length).map(_(Pattern.Rel)), dir)
   }
 
 
   class PatternRightPathBuilder(left: Pattern.Pattern0, mkRel: Pattern.Rel.Direction => Pattern.Rel) {
-    def --(node: Pattern.Node): Pattern.Path = mkPath(left, mkRel(Pattern.Rel.Any), node)
+    def --(node: Pattern.Node): Pattern.Path = mkPathBuilderPath(left, mkRel(Pattern.Rel.Any), node)
     def --( labels: Seq[String]                 = Nil,
             props:  Map[String, Known[Expr[_]]] = Map(),
             alias:  String                      = null
           ): Pattern.Path =
       this -- Pattern.Node(Option(alias), labels.toList, props)
 
-    def ->(node: Pattern.Node): Pattern.Path = mkPath(left, mkRel(Pattern.Rel.Outgoing), node)
+    def ->(node: Pattern.Node): Pattern.Path = mkPathBuilderPath(left, mkRel(Pattern.Rel.Outgoing), node)
     def ->( labels: Seq[String]                 = Nil,
             props:  Map[String, Known[Expr[_]]] = Map(),
             alias:  String                      = null
           ): Pattern.Path =
       this -> Pattern.Node(Option(alias), labels.toList, props)
+  }
 
-    private def mkPath(left: Pattern.Pattern0, rel: Pattern.Rel, right: Pattern.Node): Pattern.Path = left match {
-      case node: Pattern.Node => Pattern.Path(node, rel, right)
-      case Pattern.Path(left0, rel0, right0) => Pattern.Path(left0, rel0, mkPath(right0.fragment, rel, right))
-    }
+  class PatternLeftPathBuilder(left: Pattern.Pattern0, rel: Pattern.Rel) {
+    def --(node: Pattern.Node): Pattern.Path = mkPathBuilderPath(left, rel, node)
+    def --( labels: Seq[String]                 = Nil,
+            props:  Map[String, Known[Expr[_]]] = Map(),
+            alias:  String                      = null
+          ): Pattern.Path =
+      this -- Pattern.Node(Option(alias), labels.toList, props)
+
+    /** Unicode alias for [[--(labels:Seq[String]*]]. Has different operator precedence. */
+    def ⤙( labels: Seq[String]                 = Nil,
+            props:  Map[String, Known[Expr[_]]] = Map(),
+            alias:  String                      = null
+          ): Pattern.Path =
+      this -- Pattern.Node(Option(alias), labels.toList, props)
+  }
+
+  private def mkPathBuilderPath(left: Pattern.Pattern0, rel: Pattern.Rel, right: Pattern.Node): Pattern.Path = left match {
+    case node: Pattern.Node => Pattern.Path(node, rel, right)
+    case Pattern.Path(left0, rel0, right0) => Pattern.Path(left0, rel0, mkPathBuilderPath(right0.fragment, rel, right))
   }
 
 
