d6796fedd0a2b43bab96066878abe9227b81fc76
Dmitry Kovalev <kdn.kovalev@gmail.com>
2020-06-06 03:04:43 -0500

    [test,neo4j] Set test password to 'test'.

--- cypher/src/test/scala/com/arkondata/slothql/Connection.scala
+++ cypher/src/test/scala/com/arkondata/slothql/Connection.scala
@@ -5,7 +5,7 @@ import org.neo4j.driver.{ AuthTokens, GraphDatabase }
 object Connection {
   lazy val host = "localhost"
   lazy val port = 7687
-  lazy val auth = AuthTokens.basic( "neo4j", "neo4j" )
+  lazy val auth = AuthTokens.basic( "neo4j", "test" )
 
   def driver = GraphDatabase.driver(s"bolt://$host:$port", auth)
 }
