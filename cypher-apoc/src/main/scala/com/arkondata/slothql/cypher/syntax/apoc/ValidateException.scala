31ff07b34e9fa13930279baaa95e8c084b2efafd
Dmitry Kovalev <kdn.kovalev@gmail.com>
2020-06-13 23:21:09 -0500

    [apoc] `ValidateException` helps to extract errors thrown by `apoc.util.validate` procedure.

---
+++ cypher-apoc/src/main/scala/com/arkondata/slothql/cypher/syntax/apoc/ValidateException.scala
@@ -0,0 +1,17 @@
+package com.arkondata.slothql.cypher.syntax.apoc
+
+import org.neo4j.driver.exceptions.ClientException
+
+class ValidateException(message: String) extends Exception(message)
+
+object ValidateException {
+
+  def adapt: PartialFunction[Throwable, ValidateException] = {
+    case ce: ClientException if ce.getMessage startsWith prefix => new ValidateException(ce.getMessage.drop(prefixLength))
+  }
+
+  def unapply(err: Throwable): Option[ValidateException] = PartialFunction.condOpt(err)(adapt)
+
+  private val prefix = "Failed to invoke procedure `apoc.util.validate`: Caused by: java.lang.RuntimeException: "
+  private val prefixLength = prefix.length
+}
---
+++ cypher-apoc/src/test/scala/com/arkondata/slothql/cypher/syntax/ValidateExceptionTest.scala
@@ -0,0 +1,29 @@
+package com.arkondata.slothql.cypher.syntax
+
+import cats.syntax.monadError._
+import org.scalatest.EitherValues
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import com.arkondata.slothql.cypher.syntax.apoc.ValidateException
+import com.arkondata.slothql.test.Neo4jUsingTest
+import com.arkondata.slothql.test.tags.RequiresNeo4j
+
+@RequiresNeo4j
+class ValidateExceptionTest extends AnyWordSpec with Matchers with Neo4jUsingTest with EitherValues {
+  import tx.readers._
+
+  "Exception raised by `apoc.util.validate`" should {
+    "Be extracted/adapted from neo4j exception" in {
+      val message = "Test Assertion Failed"
+      val query = APOC.assert(lit(false), lit(message)) { lit(true) }
+      val io = tx.runRead(tx.query(query)).compile.drain
+                 .adaptError(ValidateException.adapt)
+      val result = io.attempt.unsafeRunSync()
+
+      result.left.value shouldBe an[ValidateException]
+      result.left.value.getMessage shouldBe message
+    }
+  }
+
+}
