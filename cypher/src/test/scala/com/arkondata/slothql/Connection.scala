0c296bbcc781cd3b37bd39e31c521b0f7443ce8c
Horacio Sánchez <sanchez.horacio93@gmail.com>
2021-02-12 18:31:24 -0600

    add neo4j conn

--- cypher/src/test/scala/com/arkondata/slothql/Connection.scala
+++ cypher/src/test/scala/com/arkondata/slothql/Connection.scala
@@ -3,9 +3,13 @@ package com.arkondata.slothql
 import org.neo4j.driver.{ AuthTokens, GraphDatabase }
 
 object Connection {
-  lazy val host = "localhost"
+  lazy val host = "neo4j"
   lazy val port = 7687
-  lazy val auth = AuthTokens.basic( "neo4j", "test" )
+
+  private val pattern = "(.+)/(.+)".r
+  private val pattern(username, password) = sys.env.getOrElse("NEO4J_AUTH", "neo4j/test123")
+
+  lazy val auth = AuthTokens.basic(username, password)
 
   def driver = GraphDatabase.driver(s"bolt://$host:$port", auth)
 }
