d34bab3e28cc525c1f6649e6b241d9d83fd3a1a3
Horacio Sánchez <sanchez.horacio93@gmail.com>
2022-03-02 13:38:17 -0600

    feat: complex

--- slothbie/src/test/scala/BuildStatementsSpec.scala
+++ slothbie/src/test/scala/BuildStatementsSpec.scala
@@ -1,11 +1,17 @@
+import java.util
 import java.util.UUID
 
+import scala.collection.{ mutable, Iterable }
+import scala.jdk.CollectionConverters.MapHasAsJava
+
+import org.scalatest.{ EitherValues, OptionValues }
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import com.arkondata.slothql.cypher.CypherStatement.LiftValue
 import com.arkondata.slothql.cypher.GraphElem
 
-class BuildStatementsSpec extends AnyWordSpec with Matchers {
+class BuildStatementsSpec extends AnyWordSpec with Matchers with OptionValues {
   import slothbie._
 
   "BuildStatement" should {
@@ -20,6 +26,17 @@ class BuildStatementsSpec extends AnyWordSpec with Matchers {
 
       prepared.template shouldBe "MATCH (n:Node {id: $`param0`}) return n"
       prepared.params shouldBe Map("param0" -> id)
+
+    }
+
+    "complex on create" in {
+      val id       = UUID.randomUUID()
+      val props    = Map("id" -> id)
+      val lifter   = LiftValue[Map[String, UUID]]
+      val prepared = cypher"MATCH (n:Node $props) return n".query[GraphElem.Node]
+
+      prepared.template shouldBe "MATCH (n:Node $`param0`) return n"
+      prepared.params.get("param0").value.toString shouldBe lifter.asParam(props).toString
 
     }
   }
